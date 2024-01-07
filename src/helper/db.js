e9c83fca0a73d909bffc1ae199c3865b2ae718b2
Deepak <deepak2603dk@gmail.com>
2024-01-08 00:45:31 +0530

    extanding mongo time

--- src/helper/db.js
+++ src/helper/db.js
@@ -4,7 +4,12 @@ const uri = 'mongodb://127.0.0.1:27017/TODOapp'         //for localhost mongo
 
 
 export  const connectDB= async()=>{
-
+  const mongooseOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    connectTimeoutMS: 30000, // Increase the timeout value
+    socketTimeoutMS: 30000,  // Increase the timeout value
+  };
     try {
         await mongoose.connect(uri)
         console.log('>>>>>>>>>>>DB Connected')
