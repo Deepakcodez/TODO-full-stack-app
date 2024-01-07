164c34eddec4464a70d63f55a3e221789945f86c
Deepak <deepak2603dk@gmail.com>
2024-01-08 00:44:15 +0530

    task model schema

--- src/models/task.js
+++ src/models/task.js
@@ -1,36 +1,34 @@
-
-import mongoose, {Schema}  from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const taskSchema = new Schema({
+  title: {
+    type: String,
+    minlength: 2,
+    lowercase: true,
+    required: true,
+  },
+  desc: {
+    type: String,
+    minlength: 2,
+    lowercase: true,
+    required: false,
+  },
+  addedDate: {
+    type: Date,
+    default: Date.now(),
+  },
+  status: {
+    type: String,
+    enum: ["pending", "completed"],
+    default: "pending",
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    // type : String,
+    ref: "Users", 
+    required: true,
+  },
+});
 
-    
-        title: {
-          type: String,
-          minlength: 2,
-          lowercase: true,  
-          required: true,
-        },
-        desc: {
-          type: String,
-          minlength: 2,
-          lowercase: true,  
-          required: false,
-        },
-        addedDate:{
-            type: Date,
-            default :  Date.now()
-        },
-        status: {
-            type:String,
-            enum :["pending", "completed"],
-            default : "pending",
-        },
-        userId:{
-            type : mongoose.ObjectId,
-            required : true,
-        }
-       
-       
-})
-
-export const  Tasks =  mongoose.models.Tasks  || mongoose.model("Tasks",taskSchema)
+export const Tasks =
+  mongoose.models.Tasks || mongoose.model("Tasks", taskSchema);
